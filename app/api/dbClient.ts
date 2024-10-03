50a7ecf6c95d7a14a966e2ece99b4dafaaae239c
Sivaraman S <sivasrinivasan1218@gmail.com>
2024-10-03 11:53:49 +0530

    vercel databse changed

--- app/api/dbClient.ts
+++ app/api/dbClient.ts
@@ -1,19 +1,44 @@
-import {Client} from 'pg';
+// import {Client} from 'pg';
+
+// const client = new Client({
+//     user: 'postgres',
+//     host:'localhost',
+//     database: 'music_preference_app',
+//     password: 'Siva#1207',
+//     port: 5432,
+// });
+
+// client.connect()
+// try{
+//     console.log('connected to database');
+// }
+// catch(err){
+//     console.log('Failed to connect to');
+// }
+
+// export default client;
+
+
+
+import { Client } from 'pg';
 
 const client = new Client({
-    user: 'postgres',
-    host:'localhost',
-    database: 'music_preference_app',
-    password: 'Siva#1207',
+    user: 'default', // Vercel username
+    host: 'ep-soft-wave-a4243c36-pooler.us-east-1.aws.neon.tech', // Vercel host
+    database: 'verceldb', // Vercel database name
+    password: 'Sdzc2fwy5hKt', // Vercel password
     port: 5432,
+    ssl: {
+        rejectUnauthorized: false // Important for Vercel connection
+    }
 });
 
-client.connect()
-try{
-    console.log('connected to database');
-}
-catch(err){
-    console.log('Failed to connect to');
-}
+client.connect(err => {
+    if (err) {
+        console.error('Failed to connect to the database:', err);
+    } else {
+        console.log('Connected to the database');
+    }
+});
 
 export default client;
